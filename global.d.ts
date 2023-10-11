58865b4344778d0e02b5e9762870073cf0ca59aa
Ethaks <ethaks@protonmail.ch>
2023-10-11 16:10:26 +0200

    chore: remove old weapon-attack file, enable global usage of document types

--- global.d.ts
+++ global.d.ts
@@ -3,6 +3,10 @@ import * as _outerheaven from "./outerheaven.mjs";
 export {};
 
 declare global {
+    // Documents
+    export import OHActor = _outerheaven.documents.OHActor;
+    export import OHItem = _outerheaven.documents.OHItem;
+
     type DamageType = keyof typeof outerheaven.config.damageTypes;
     // Action types
     export import ItemReloadAction = _outerheaven.actions.ReloadAction;
