d25a04c23678a8d56a99584903b5c776df2789a8
Ethaks <ethaks@protonmail.ch>
2023-11-16 19:24:04 +0100

    fix: allow new packs as item compendium values

--- module/data/settings.mjs
+++ module/data/settings.mjs
@@ -6,17 +6,23 @@ export class ItemCompendiumSettings extends foundry.abstract.DataModel {
     static defineSchema() {
         const fields = foundry.data.fields;
         // Use game.data to enable verification of fields even when Foundry is not ready yet
-        const choices = Array.from(
-            new Set([...game.packs, ...game.data.packs].map((pack) => pack.id ?? pack.metadata.id)),
-        );
         return game.system.template.Item.types.reduce((acc, type) => {
             acc[type] = new fields.StringField({
                 initial: "",
                 nullable: false,
                 blank: true,
-                choices,
+                choices: this.getChoices,
             });
             return acc;
         }, {});
+    }
+
+    /**
+     * Get a list of currently available compendiums.
+     *
+     * @returns {string[]} The list of available compendiums.
+     */
+    static getChoices() {
+        return Array.from(new Set([...game.packs, ...game.data.packs].map((pack) => pack.id ?? pack.metadata?.id)));
     }
 }
